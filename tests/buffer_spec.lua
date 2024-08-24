8441ce10fce80925b472c843428905923599a617
worldsayshi <per.fredelius+git@gmail.com>
2024-08-24 23:39:13 +0200

    fixed delay between tests

--- tests/buffer_spec.lua
+++ tests/buffer_spec.lua
@@ -1,28 +1,18 @@
-print("very top ot the test file")
-print("beginning of test file")
-
 describe("stream_command_to_floating_buffer", function()
 	local M = require("buffer")
 	-- local helpers = require('plenary.test_harness.helpers')
 	local eq = assert.are.same
 	it("streams the output of a command to a buffer", function()
-		print("starting the test")
-		-- Create a new buffer to simulate the original buffer
-		-- local original_bufnr = vim.api.nvim_create_buf(false, true)
+		local command_finished = false
 
 		-- Define the command to be executed
 		local cmd = { "echo", "Hello, World!" }
-
-		print("starting the function")
-		-- Call the function
-		M.stream_command_to_floating_buffer(cmd, nil)
-
-		print("starting the wait")
-		-- Wait for the command to complete
-		vim.wait(100, function()
-			return vim.fn.jobwait({ vim.fn.jobstart(cmd) }, 0)[1] ~= -1
+		M.stream_command_to_floating_buffer(cmd, function()
+			command_finished = true
 		end)
-
+		vim.wait(5000, function()
+			return command_finished
+		end)
 		-- Get the contents of the floating buffer
 		local buf_lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
 
