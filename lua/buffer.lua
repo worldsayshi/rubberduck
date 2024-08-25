e8636ca95812bd50f3e8f5e2b21c6fd20c12fe8f
worldsayshi <per.fredelius+git@gmail.com>
2024-08-25 17:15:02 +0200

    cleanup

--- lua/buffer.lua
+++ lua/buffer.lua
@@ -1,4 +1,3 @@
-local dump = require("dump")
 local M = {}
 
 M.create_floating_buffer = function()
@@ -28,18 +27,6 @@ M.create_floating_buffer = function()
 			vim.schedule(function()
 				vim.api.nvim_set_option_value("modifiable", true, { buf = buf })
 				vim.api.nvim_buf_set_lines(buf, -1, -1, false, data)
-				-- local last_line = vim.api.nvim_buf_get_lines(buf, -2, -1, false)[1] or ""
-				-- for _, line in ipairs(data) do
-				-- 	if line:sub(-1) == "\n" then
-				-- 		vim.api.nvim_buf_set_lines(buf, -2, -1, false, { last_line .. line:sub(1, -2) })
-				-- 		last_line = ""
-				-- 	else
-				-- 		last_line = last_line .. line
-				-- 	end
-				-- end
-				-- if last_line ~= "" then
-				-- 	vim.api.nvim_buf_set_lines(buf, -2, -1, false, { last_line })
-				-- end
 				vim.api.nvim_set_option_value("modifiable", false, { buf = buf })
 
 				local line_count = vim.api.nvim_buf_line_count(buf)
